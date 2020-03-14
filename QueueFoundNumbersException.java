4652182975acb7a9f62cb64337a37d44c15d7d9b
FScholsen <fscholsen@gmail.com>
2020-03-14 06:32:31 +0100

    add new exception to stop at max numbers found

---
+++ QueueFoundNumbersException.java
@@ -0,0 +1,34 @@
+
+public class QueueFoundNumbersException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7244019817040452859L;
+
+	public QueueFoundNumbersException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public QueueFoundNumbersException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QueueFoundNumbersException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QueueFoundNumbersException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QueueFoundNumbersException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
